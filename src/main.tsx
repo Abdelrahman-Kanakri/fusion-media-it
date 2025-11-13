dafe170ea0b10a02eb61f40e6f63eda592fdb640
Abood Kanakri <99169170+Abdelrahman-Kanakri@users.noreply.github.com>
2025-11-13 15:54:51 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom"; // 1. Import HashRouter
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+// 2. Wrap your <App /> component in <HashRouter>
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
